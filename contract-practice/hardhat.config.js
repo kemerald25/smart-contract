51762d0a4b992c30f961b24f5bd7ca4b64f34c7f
Kemerald25 <unityekeoba18@gmail.com>
2024-04-25 04:07:25 +0100

    updated api

--- contract-practice/hardhat.config.js
+++ contract-practice/hardhat.config.js
@@ -4,4 +4,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `${process.env.ALCHEMY_SEPOLIA_URL}`,
+      accounts: [`0x${process.env.SEPOLIA_PRIVATE_KEY}`],
+    }
+  }
 };
